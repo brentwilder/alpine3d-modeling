685cbe7c9fe63bbd3d317daaa6cf17b146964e13
Brent Wilder <56372982+brentwilder@users.noreply.github.com>
2022-04-27 12:30:58 -0600

    Create compute_forcings.py

---
+++ compute_forcings.py
@@ -0,0 +1,9 @@
+# Script to prep forcings for Alpine3D 
+# Brent Wilder
+# 04/26/2022
+
+# Things it will do:
+# Convert specific humidity to relative humidity
+# Calculate WS vector from U10 and V10 wind data
+# Bias correct air temp and precip with PRISM
+# Output all variables: TA, RH, WS, PSUM, ISWR, ILWR
