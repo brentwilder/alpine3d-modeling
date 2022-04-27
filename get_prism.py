aa9f0c1273a1682714b834fa50db947026fa840f
Brent Wilder <56372982+brentwilder@users.noreply.github.com>
2022-04-27 12:33:25 -0600

    Create get_prism.py

---
+++ get_prism.py
@@ -0,0 +1,10 @@
+# Get PRISM data for bias correcting temperature and precip in next script
+# Brent Wilder
+# 04/26/2022
+
+PRISMDataLoc = 'ftp://prism.nacse.org/monthly'
+
+prism_ppt_version = '3'                           
+prism_tmean_version = '3'        
+
+# TODO: similar to get_nldas2 script, call to get all of the data
