c966520e94c5ff02bc44c4b9b165675e6dc2ac78
Brent Wilder <56372982+brentwilder@users.noreply.github.com>
2022-04-28 13:52:45 -0600

    additional comments on game plan

--- compute_forcings.py
+++ compute_forcings.py
@@ -7,3 +7,10 @@
 # Calculate WS vector from U10 and V10 wind data
 # Bias correct air temp and precip with PRISM
 # Output all variables: TA, RH, WS, PSUM, ISWR, ILWR
+
+# NOTE: for PRISM bias correction
+# Output TA and PSUM from PRISM correction
+# PRISM (match cumulative precip, monthly average temp)
+# BW EDIT, PRISM is in C and NLDAS is in K.
+# Will need to devise a way to group data monthly... and loop through.
+# also keep in mind that PRISM at 4KM and NLDAS is 10KM (match NLDAS)
