ff9c582647de0e911d676d13d3e3959918651cd1
Brent Wilder <56372982+brentwilder@users.noreply.github.com>
2022-04-28 17:30:31 -0600

    Update compute_forcings.py

--- compute_forcings.py
+++ compute_forcings.py
@@ -3,14 +3,7 @@
 # 04/26/2022
 
 # Things it will do:
-# Convert specific humidity to relative humidity
-# Calculate WS vector from U10 and V10 wind data
-# Bias correct air temp and precip with PRISM
-# Output all variables: TA, RH, WS, PSUM, ISWR, ILWR
-
-# NOTE: for PRISM bias correction
-# Output TA and PSUM from PRISM correction
-# PRISM (match cumulative precip, monthly average temp)
-# BW EDIT, PRISM is in C and NLDAS is in K.
-# Will need to devise a way to group data monthly... and loop through.
-# also keep in mind that PRISM at 4KM and NLDAS is 10KM (match NLDAS)
+# Calculate VW vector from U10 and V10 wind data
+# Then need to downscale data to 30 meter to match DEM file
+# and then finally clip this file to the study domain bounds
+# Output all variables: TA, RH, VW, PSUM, ISWR, ILWR
