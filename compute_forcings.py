da251fc1b32dcb90b74892dc4c56f21d484586d7
Brent Wilder <56372982+brentwilder@users.noreply.github.com>
2022-05-03 17:01:26 -0600

    Update compute_forcings.py

--- compute_forcings.py
+++ compute_forcings.py
@@ -3,7 +3,8 @@
 # 04/26/2022
 
 # Things it will do:
-# Calculate VW vector from U10 and V10 wind data
-# Then need to downscale data to 30 meter to match DEM file
-# and then finally clip this file to the study domain bounds
+# USE MetPy to :
+#   Calc VW vector from U10 and V10 wind data (later to be ran through WindNinja)
+#   Calc RH from Air T, Pressure, and SH
+# Downscale all of these data to 30 meters to match model domain
 # Output all variables: TA, RH, VW, PSUM, ISWR, ILWR
