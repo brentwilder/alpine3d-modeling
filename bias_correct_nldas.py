f3ad68d73418ae61555434ed683012eb18873eaa
Brent Wilder <56372982+brentwilder@users.noreply.github.com>
2022-05-04 09:47:34 -0600

    Update bias_correct_nldas.py

--- bias_correct_nldas.py
+++ bias_correct_nldas.py
@@ -1,10 +1,17 @@
-# Script needs to do the following,
-# loop through all of nldas data to:
-#   for the given month, first convert prism file to netcdf
-#   match nldas scale/extent to prism
-#   take average for each day (24 hours)
-#   of the daily avgs, take monthly average for those values
-#   compare those monthly averages to prism to find bias
-#   "add" this bias correction to the nldas netcdf
-#   keep all other variables within and write these to new folder
-# TODO: also, would be neat to save this air temp bias correction for reference later for write up
+# Bias correct NLDAS-2 air temperature using monthly PRISM
+# Brent Wilder
+# 05/04/2022
+
+# Call in merged nc file
+
+
+# Match scale and extent of first prism dataset in the folder (4km)
+
+
+# Create a new variable that is the average monthly Air Temp
+
+
+# Create a new variable that is the difference between prism and nldas (bias)
+
+
+# "Add" this bias to create a new corrected air temp variable
