37cf252938ea077acd916c3d5c941e60e1177bd7
Brent Wilder <56372982+brentwilder@users.noreply.github.com>
2022-05-04 00:32:24 -0600

    Update compute_forcings.py

--- compute_forcings.py
+++ compute_forcings.py
@@ -2,9 +2,12 @@
 # Brent Wilder
 # 04/26/2022
 
-# Things it will do:
-# USE MetPy to :
-#   Calc VW vector from U10 and V10 wind data (later to be ran through WindNinja)
-#   Calc RH from Air T, Pressure, and SH
-# Downscale all of these data to 30 meters to match model domain
+# From here loading in the prism_corrected_nldas:
+#   Then, use MetPy to :
+#       Calc VW vector from U10 and V10 wind data (later to be ran through WindNinja)
+#       return VW to folder
+
+#   MetPy Calc RH from Air T, Pressure, and SH
+#       return RH
+
 # Output all variables: TA, RH, VW, PSUM, ISWR, ILWR
