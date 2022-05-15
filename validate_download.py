bd90aa5211f380ac97fd8f9b4eae2a10731d7724
Brent Wilder <56372982+brentwilder@users.noreply.github.com>
2022-05-15 02:46:52 -0600

    Update validate_download.py

--- validate_download.py
+++ validate_download.py
@@ -1,16 +1,25 @@
-# CHECK FOR DUPLICATES TOO!!!! 
+# Finds missing files and exports a list to csv
+# This csv can then be fed to get_nldas2_leftover.py
+# to catch any missing files...
+# Can also check duplicates if for some reason you downloaded a multiple times
+# Brent Wilder
+# 05/15/22
+
+# Import libraries
 from pathlib import Path
 from datetime import datetime,timedelta
 import pandas as pd
 
+# Set start and end dates
 startdate = datetime(1981, 10, 1, 0)
 enddate = datetime(2021, 10, 1, 0)
-df = pd.DataFrame(columns=['file'])
-files=[]
 
-# loop through time to output all corrected hourly TA
+# Initate empty lists for verification and duplicates
+files=[]
+duplicates=[]
+
+# loop through time 
 while startdate < enddate:
-    # There's probably a better way to do this....
     yr = str(startdate.year)
     mo = startdate.month
     if mo <= 9:
@@ -31,13 +40,24 @@ while startdate < enddate:
     # test file
     myfile = Path('./nldas/NLDAS_FORA0125_H.A'+yr+mo+dy+'.'+hr+'00.020.nc')
 
-    # write file if does not exist
+    # write file if does not exist yet..
     if not myfile.is_file():
         files.append([myfile])
+
+    # Check if there is a duplicate
+    for i in range(1,5):
+        myfile_duplicate = Path('./nldas/NLDAS_FORA0125_H.A'+yr+mo+dy+'.'+hr+'00.020('+str(i)+').nc')
+        if myfile_duplicate.is_file():
+            duplicates.append([myfile_duplicate])
 
     # Jump back up to top of main loop to do next hour
     startdate = startdate + timedelta(hours=1)
 
- # Save it to a dataframe
+# Save them to dataframes
+df = pd.DataFrame(columns=['file'])
 df = pd.DataFrame(files,columns=['file'])
 df.to_csv('./verify.csv')
+
+df = pd.DataFrame(columns=['file'])
+df = pd.DataFrame(duplicates,columns=['file'])
+df.to_csv('./duplicates.csv')
